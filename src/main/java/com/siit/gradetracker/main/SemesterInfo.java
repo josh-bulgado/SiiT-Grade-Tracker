12b2e0919ce740c67a4740b47dc1f62b6dc3d520
josh-bulgado <j.ll.bulgado@gmail.com>
2024-12-01 14:33:32 +0800

    Add overallUnits

--- src/main/java/com/siit/gradetracker/main/SemesterInfo.java
+++ src/main/java/com/siit/gradetracker/main/SemesterInfo.java
@@ -5,10 +5,20 @@ import java.util.List;
 public class SemesterInfo {
   private List<Course> courses;
   private double gwa;
+  private int overAllUnits;
 
-  public SemesterInfo(List<Course> courses, double gwa) {
+  public SemesterInfo(List<Course> courses, double gwa, int overaAllUnits) {
     this.courses = courses;
     this.gwa = gwa;
+    this.overAllUnits = overaAllUnits;
+  }
+
+  public void setGwa(double gwa) {
+    this.gwa = gwa;
+  }
+
+  public void setOverAllUnits(int overaAllUnits) {
+    this.overAllUnits = overaAllUnits;
   }
 
   public List<Course> getCourses() {
@@ -19,7 +29,8 @@ public class SemesterInfo {
     return gwa;
   }
 
-  public void setGwa(double gwa) {
-    this.gwa = gwa;
+  public int getOverAllUnits() {
+    return overAllUnits;
   }
+
 }
